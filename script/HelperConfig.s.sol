09656ea9e06a93755173cd44317ab818e8b01186
alexandre-pecorilla <alexandre@pecorilla.us>
2026-03-24 22:28:12 -0400

    Mock contract and refactoring

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+// Deploy mocks when we are on a local anvil chain
+// keep track of the contract addresses across the different chains
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD Price Feed contract address
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            // check if the current chain is sepolia
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    // get the chainlink Price Feed contract address on Sepolia
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    // get the mock Price Feed contract address on Anvil
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        // Deploy the mock
+
+        // Return the mock address
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
+}
